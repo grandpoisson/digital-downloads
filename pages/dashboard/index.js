3a938c3b8ee04f9fbfe97024986ba3ea0a491592
grandpoisson <grandpoisson@gmail.com>
2022-07-25 22:01:39 +0300

    implement checkout via stripe

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,94 +1,143 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { useSession, getSession } from 'next-auth/react'
-import Link from 'next/link'
-import Heading from 'components/Heading'
-import prisma from 'lib/prisma'
-import { getProducts } from 'lib/data'
+import { useRouter } from "next/router";
+import { useSession, getSession } from "next-auth/react";
+import Link from "next/link";
+import Head from "next/head";
+import Heading from "components/Heading";
 
-export default function Dashboard({ products }) {
-  const { data: session, status } = useSession()
-  const router = useRouter()
+import prisma from "lib/prisma";
+import { getProducts, getPurchases } from "lib/data";
 
-  const loading = status === 'loading'
+export default function Dashboard({ products, purchases }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  const loading = status === "loading";
 
   if (loading) {
-    return null
+    return null;
   }
 
   if (!session) {
-    router.push('/')
+    router.push("/");
   }
 
   if (session && !session.user.name) {
-    router.push('/setup')
+    router.push("/setup");
   }
 
   return (
     <div>
-        <Head>
+      <Head>
         <title>Digital Downloads</title>
-        <meta name='description' content='Digital Downloads Website' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Digital Downloads Website" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Heading />
 
-      <h1 className='flex justify-center mt-20 text-xl'>Dashboard</h1>
+      <h1 className="flex justify-center mt-20 text-xl">Dashboard</h1>
 
-      <div className='flex justify-center mt-10'>
+      <div className="flex justify-center mt-10">
         <Link href={`/dashboard/new`}>
-          <a className='text-xl border p-2'>Create a new product</a>
+          <a className="text-xl border p-2">Create a new product</a>
         </Link>
       </div>
 
-      <div className='flex justify-center mt-10'>
-        <div className='flex flex-col w-full '>
-          {products && products.map((product, index) => (
-            <div
-              className='border flex justify-between w-full md:w-2/3 xl:w-1/3 mx-auto px-4 my-2 py-5 '
-              key={index}
-            >
-              {product.image && (
-                <img src={product.image} className='w-14 h-14 flex-initial' />
-              )}
-              <div className='flex-1 ml-3'>
-                <p>{product.title}</p>
-                {product.free ? (
-                  <span className='bg-white text-black px-1 uppercase font-bold'>
-                    free
-                  </span>
-                ) : (
-                  <p>${product.price / 100}</p>
-                )}
-              </div>
-              <div className=''>
-                <Link href={`/dashboard/product/${product.id}`}>
-                  <a className='text-sm border p-2 font-bold uppercase'>Edit</a>
-                </Link>
-                <Link href={`/product/${product.id}`}>
-                    <a className='text-sm border p-2 font-bold uppercase ml-2'>View</a>
-                </Link>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <div className="flex justify-center mt-10">
+        {products.length > 0 && (
+          <div className="flex flex-col w-full ">
+            <h2 className="text-center text-xl mb-4">Products</h2>
 
+            {products.map((product, index) => (
+              <div
+                className="border flex justify-between w-full md:w-2/3 xl:w-1/3 mx-auto px-4 my-2 py-5 "
+                key={index}
+              >
+                {product.image && (
+                  <img src={product.image} className="w-14 h-14 flex-initial" />
+                )}
+                <div className="flex-1 ml-3">
+                  <p>{product.title}</p>
+                  {product.free ? (
+                    <span className="bg-white text-black px-1 uppercase font-bold">
+                      free
+                    </span>
+                  ) : (
+                    <p>${product.price / 100}</p>
+                  )}
+                </div>
+                <div className="">
+                  <Link href={`/dashboard/product/${product.id}`}>
+                    <a className="text-sm border p-2 font-bold uppercase">
+                      Edit
+                    </a>
+                  </Link>
+                  <Link href={`/product/${product.id}`}>
+                    <a className="text-sm border p-2 font-bold uppercase ml-2">
+                      View
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+
+        {purchases.length > 0 && (
+          <div className="flex flex-col w-full">
+            <h2 className="text-center text-xl mb-4">Purchases</h2>
+            {purchases.map((purchase, index) => (
+              <div
+                className="border flex justify-between w-full md:w-2/3 xl:w-1/3 mx-auto px-4 my-2 py-5 "
+                key={index}
+              >
+                {purchase.product.image && (
+                  <img
+                    src={purchase.product.image}
+                    className="w-14 h-14 flex-initial"
+                  />
+                )}
+                <div className="flex-1 ml-3">
+                  <p>{purchase.product.title}</p>
+                  {parseInt(purchase.amount) === 0 ? (
+                    <span className="bg-white text-black px-1 uppercase font-bold">
+                      free
+                    </span>
+                  ) : (
+                    <p>${purchase.amount / 100}</p>
+                  )}
+                </div>
+                <div className="">
+                  <a
+                    href={purchase.product.url}
+                    className="text-sm border p-2 font-bold uppercase"
+                  >
+                    Get files
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
-    const session = await getSession(context)
+  const session = await getSession(context);
+  if (!session) return { props: {} };
 
-    if (!session) return { props: {} }
-    let products = await getProducts({ author: session.user.id }, prisma)
-	products = JSON.parse(JSON.stringify(products))
+  let products = await getProducts({ author: session.user.id }, prisma);
+  products = JSON.parse(JSON.stringify(products));
 
-    return {
-      props: {
-        products,
-      }
-    }
-  }
+  let purchases = await getPurchases({ author: session.user.id }, prisma);
+  purchases = JSON.parse(JSON.stringify(purchases));
+
+  return {
+    props: {
+      products,
+      purchases,
+    },
+  };
+}
